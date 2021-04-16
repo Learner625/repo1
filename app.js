3f8863e722fc127fa661545d864e7382b64b9254
Learner625 <studylearning592@gmail>
2021-04-16 21:25:15 +0600

    My first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+       document.querySelector('.netlify-identity-signup').addEventListener('click',function(){
+              ducument.querySelector('.modalContent').classList.add('show');
+       })
+       
+      if (emailinsignup.value==emailinlogin.value)
+       {
+       document.querySelector('.classnameinlogin.htmlpage').
+       innerHTML='usernameinsignup.value';
+       document.querySelector('.classnameinlogin.htmlpage').
+       innerHTML='username'+'usernameinsignup.value';
+       document.querySelector('.classnameinlogin.htmlpage').
+       innerHTML='email'+'usernameinlogin.value';
+       document.querySelector('.passwordinlogin.htmlpage').
+       innerHTML='password'+'passwordinlogin.value';
+       }
+       
